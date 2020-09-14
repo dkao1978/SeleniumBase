d36ce97b447303662264492b9d32d8df6a2a3f06
Dean Kao <dean@eudogames.com>
2020-09-14 12:24:38 -0700

    Fix dependencies

--- setup.py
+++ setup.py
@@ -129,7 +129,6 @@ setup(
         'pytest-xdist==2.1.0;python_version>="3.5"',
         'parameterized==0.7.4',
         'soupsieve==1.9.6;python_version<"3.5"',
-        'soupsieve==2.0.1;python_version>="3.5"',
         'beautifulsoup4==4.9.1',
         'cryptography==3.0;python_version<"3.6"',
         'cryptography==3.1;python_version>="3.6"',
